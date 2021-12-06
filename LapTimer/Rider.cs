cab7b197ad7220a0dce768bb6d9e48b27a4274a7
lornop <90425445+lornop@users.noreply.github.com>
2021-12-06 11:19:50 -0800

    Did some formatting in xaml. Still sucks
    Need to start coding the timer and stuff

--- LapTimer/Rider.cs
+++ LapTimer/Rider.cs
@@ -17,10 +17,7 @@ namespace LapTimer
         public int LapNumber;
 
 
-        //public void RiderTimer()
-        //{
-        //    tim;
-        //}
+
 
         /// <summary>
         /// This timer is started when the player clicks 
@@ -28,23 +25,15 @@ namespace LapTimer
         /// so it counts three quarters of a second 
         /// and then turns itself off and hides both icons
         /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="e"></param>
-        //private void timer1_Tick(object sender, EventArgs e)
-        //{
-        //    // Stop the timer
-        //    timer1.Stop();
+        /// 
 
-        //    // Hide both icons
-        //    firstClicked.ForeColor = firstClicked.BackColor;
-        //    secondClicked.ForeColor = secondClicked.BackColor;
+        public void RiderTimer()
+        {
+            time;
+        }
 
-        //    // Reset firstClicked and secondClicked 
-        //    // so the next time a label is
-        //    // clicked, the program knows it's the first click
-        //    firstClicked = null;
-        //    secondClicked = null;
-        //}
+
+
 
 
     }
